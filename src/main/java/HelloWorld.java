233e4fd09b7bfde82bb193a04db2820e710b35d2
techyavi <99551194+techyavixyz@users.noreply.github.com>
2024-03-22 11:59:32 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 import spark.servlet.SparkApplication;
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public class HelloWorld implements SparkApplication {
     public static void main(String[] args) {
