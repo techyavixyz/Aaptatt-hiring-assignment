0847d1e57c512b525c54097297945769c1f24fd1
techyavi <99551194+techyavixyz@users.noreply.github.com>
2024-03-22 14:15:04 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,8 @@
 import spark.servlet.SparkApplication;
-import static spark.Spark.*;
+
+import static spark.Spark.get;
 
 public class HelloWorld implements SparkApplication {
-    public static void main(String[] args) {
-        new HelloWorld().init();
-    }
-
     @Override
     public void init() {
         get("/aaptatt", (req, res) -> "Welcome to Aaptatt!!");
