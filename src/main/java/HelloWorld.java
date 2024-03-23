e4922510a9574f66647117d8ffb14bc99bd029ff
techyavi <99551194+techyavixyz@users.noreply.github.com>
2024-03-23 16:38:04 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,12 +3,29 @@ import spark.servlet.SparkApplication;
 import static spark.Spark.get;
 
 public class HelloWorld implements SparkApplication {
-	public static void main(String[] args) {
-		new HelloWorld().init();
-	}
+    public static void main(String[] args) {
+        new HelloWorld().init();
+    }
 
-	@Override
-	public void init() {
-		get("/aaptatt", (req, res) -> "Welcome to Aaptatt!!");
-	}
+    @Override
+    public void init() {
+        get("/techyavi", (req, res) -> {
+            String html = "<!DOCTYPE html>" +
+                    "<html lang=\"en\">" +
+                    "<head>" +
+                    "<meta charset=\"UTF-8\">" +
+                    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
+                    "<title>Welcome to TechyAvi!!</title>" +
+                    "<link rel=\"stylesheet\" href=\"/path/to/style.css\">" +  // Change the path accordingly
+                    "</head>" +
+                    "<body>" +
+                    "<div class=\"container\">" +
+                    "<h1>Welcome to Aaptatt!!</h1>" +
+                    "</div>" +
+                    "</body>" +
+                    "</html>";
+            res.type("text/html");
+            return html;
+        });
+    }
 }
