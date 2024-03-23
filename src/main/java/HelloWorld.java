0112aad607758eb423e27333758e74cc7c9de5b3
techyavi <99551194+techyavixyz@users.noreply.github.com>
2024-03-23 16:42:38 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -16,12 +16,27 @@ public class HelloWorld implements SparkApplication {
                     "<meta charset=\"UTF-8\">" +
                     "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" +
                     "<title>Welcome to TechyAvi!!</title>" +
-                    "<link rel=\"stylesheet\" href=\"/path/to/style.css\">" +  // Change the path accordingly
+                    "<style>" +
+                    "body, html {" +
+                    "    margin: 0;" +
+                    "    padding: 0;" +
+                    "    height: 100%;" +
+                    "    overflow: hidden;" +
+                    "}" +
+                    "img {" +
+                    "    position: absolute;" +
+                    "    top: 50%;" +
+                    "    left: 50%;" +
+                    "    transform: translate(-50%, -50%);" +
+                    "    min-width: 100%;" +
+                    "    min-height: 100%;" +
+                    "    width: auto;" +
+                    "    height: auto;" +
+                    "}" +
+                    "</style>" +
                     "</head>" +
                     "<body>" +
-                    "<div class=\"container\">" +
-                    "<h1>Welcome to Aaptatt!!</h1>" +
-                    "</div>" +
+                    "<img src=\"https://github.com/techyavixyz/SparkApplication/raw/master/Happy%20hoil.gif\" alt=\"Autoplaying GIF\">" +
                     "</body>" +
                     "</html>";
             res.type("text/html");
